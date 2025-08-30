2d595d2cf29bea8cd139178b2c5b3660083171b3
Ferran Plana Rius <nandoplana@gmail.com>
2025-08-30 10:46:27 +0200

    fidget transparency bug solved

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,34 +8,6 @@ return {
   {
     'xiyaowong/transparent.nvim',
     lazy = false,
-    -- table: default groups
-    groups = {
-      'Normal',
-      'NormalNC',
-      'Comment',
-      'Constant',
-      'Special',
-      'Identifier',
-      'Statement',
-      'PreProc',
-      'Type',
-      'Underlined',
-      'Todo',
-      'String',
-      'Function',
-      'Conditional',
-      'Repeat',
-      'Operator',
-      'Structure',
-      'LineNr',
-      'NonText',
-      'SignColumn',
-      'CursorLine',
-      'CursorLineNr',
-      'StatusLine',
-      'StatusLineNC',
-      'EndOfBuffer',
-    },
     -- table: additional groups that should be cleared
     extra_groups = {},
     -- table: groups you don't want to clear
@@ -46,7 +18,7 @@ return {
   },
   {
     'folke/snacks.nvim',
-    priority=1000,
+    priority = 1000,
     ---@type snacks.Config
     opts = {
       notifier = {
